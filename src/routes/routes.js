bd656d29705ca986144dd38584144958e832715a
lararomeroo <laracristianoromero11122007@gmail.com>
2025-06-24 11:39:42 -0300

    tsete

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -2,7 +2,17 @@ const express = require('express');
 const router = express.Router(); 
 
 const RotasLara = require('./routes-lara'); 
+const RotasLara = require('./routes-giulia'); 
+const RotasLara = require('./routes-laura'); 
+const RotasLara = require('./routes-stefany'); 
+const RotasLara = require('./routes-raphael'); 
+const RotasLara = require('./routes-mariajulia'); 
 
 router.use('/', RotasLara);
+router.use('/', RotasGiulia);
+router.use('/', RotasLaura);
+router.use('/', RotasStefany);
+router.use('/', RotasRaphael);
+router.use('/', RotasMariajulia);
 
 module.exports = router;

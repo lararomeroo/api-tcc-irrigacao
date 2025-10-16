a6bd97b19ee74026e3de46a35bf335a3c903ff38
lararomeroo <160646974+lararomeroo@users.noreply.github.com>
2025-10-16 10:57:34 -0300

    login

--- src/routes/routes-giulia.js
+++ src/routes/routes-giulia.js
@@ -7,6 +7,7 @@ router.get('/usuarios', UsuarioController.listarUsuarios);
 router.post('/usuarios', UsuarioController.cadastrarUsuarios); 
 router.patch('/usuarios/:id_usu', UsuarioController.editarUsuarios); 
 router.delete('/usuarios/:id_usu', UsuarioController.apagarUsuarios); 
+router.get('/login', UsuarioController.login);
 
 
 module.exports = router;
